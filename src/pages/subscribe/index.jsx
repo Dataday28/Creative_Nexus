82b893bea757e9d32b1a0688d7230173a2bf439d
Marco Castañon <dataday-rocker@hotmail.com>
2024-04-13 17:29:31 -0600

    Se instalo React-Hook-Form

--- src/pages/subscribe/index.jsx
+++ src/pages/subscribe/index.jsx
@@ -1,5 +1,14 @@
+import Footer from "../../components/Footer";
+import NavBar from "../../components/Navbar";
+
 const Subscribe = () => {
-    return <div>Subscribe</div>
+    return (
+        <div>
+            <NavBar />
+            <h1>Subscribe</h1>
+            <Footer />
+        </div>
+    )
 }
 
 export default Subscribe;
